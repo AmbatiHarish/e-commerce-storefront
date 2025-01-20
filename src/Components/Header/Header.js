018d012e593a3a51a5897d70d914b60a4f5453a1
AmbatiHarish <40839012+AmbatiHarish@users.noreply.github.com>
2025-01-19 23:25:03 -0800

    Created Header Component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className='HeaderContainer'>
+            <h1>E-Commerce Store</h1>
+            <nav>
+                <Link to="/" className='HeaderLink'>Home</Link>
+                <Link to="/cart" className='HeaderLink'>Cart</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
