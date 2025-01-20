35630007161571d217967e1a4a00b4e8cf418fc2
AmbatiHarish <40839012+AmbatiHarish@users.noreply.github.com>
2025-01-19 23:35:14 -0800

    Added Router Navigation

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className='HeaderContainer'>
+        <header className="HeaderContainer">
             <h1>E-Commerce Store</h1>
             <nav>
-                <Link to="/" className='HeaderLink'>Home</Link>
-                <Link to="/cart" className='HeaderLink'>Cart</Link>
+                <Link to="/" className="HeaderLink">Home</Link>
+                <Link to="/cart" className="HeaderLink">Cart</Link>
             </nav>
         </header>
     );
